6a04118a76995ff2870520391f1f428f1f4aebac
Sayedul Sayem <sayedulsayem@gmail.com>
2024-05-01 03:18:28 +0600

    remove unwanted js library

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
-import AutoImport from 'unplugin-auto-import/vite'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
 export default defineConfig({
     plugins: [
-        AutoImport({
-            resolvers: [ ElementPlusResolver() ],
-        }),
-        Components({
-            resolvers: [ ElementPlusResolver() ],
-        }),
         laravel({
             input: [ 'resources/css/app.css', 'resources/js/app.js' ],
             refresh: true,
